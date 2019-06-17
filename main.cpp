e028324764c7422c252b8d4d150ab147717dca15
Max Base <MaxBaseCode@Gmail.Com>
2019-06-17 17:25:10 +0430

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/**
+*
+* @Name : MatrixFunction/main.cpp
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-07-17
+* @Released under : https://github.com/BaseMax/MatrixFunction/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/MatrixFunction
+*
+**/
