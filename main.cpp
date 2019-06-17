e57ae50781fa3a23d63f9e2c627a9f9653b76c9d
Max Base <MaxBaseCode@Gmail.Com>
2019-06-17 17:25:26 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,3 +8,15 @@
 * @Repository : https://github.com/BaseMax/MatrixFunction
 *
 **/
+#include <iostream>
+// #include <math.h>
+#include <cmath>
+/*
+ * Reffrences
+ * @url:  http://www.cplusplus.com/doc/tutorial/classes/
+ * @url: https://en.wikipedia.org/wiki/Determinant
+ * @url: https://en.wikipedia.org/wiki/coefficient
+ * @url: https://en.wikipedia.org/wiki/Matrix_multiplication
+ * @url: https://en.wikipedia.org/wiki/Cramer%27s_rule
+ * @url: https://fa.wikipedia.org/wiki/%D9%82%D8%A7%D8%B9%D8%AF%D9%87_%DA%A9%D8%B1%D8%A7%D9%85%D8%B1
+*/
