12930dbeb152cc044140e4b9623679db1cb43038
Max Base <MaxBaseCode@Gmail.Com>
2019-06-17 17:33:59 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 *
 * @Name : MatrixFunction/main.cpp
 * @Version : 1.0
-* @Programmer : Max
+* @Programmer : SorenCoder, Max
 * @Date : 2019-07-17
 * @Released under : https://github.com/BaseMax/MatrixFunction/blob/master/LICENSE
 * @Repository : https://github.com/BaseMax/MatrixFunction
